1796bf4bd458541ad37fb2841585cfe9f5cc559a
pinshypinshy <hrisyklit@gmail.com>
2026-02-08 15:33:52 +0900

    Initial Commit

---
+++ Icon/IconApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconApp.swift
+//  Icon
+//
+//  Created by 平石悠生 on 2026/02/08.
+//
+
+import SwiftUI
+
+@main
+struct IconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
